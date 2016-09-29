9ddb73378390528e1a893516e07dd63d12d433db
Adam Zolotarev <eugenezolo@gmail.com>
2016-09-28 22:28:53 -0400

    Show movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'events/movies'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'movies' => 'movies#index'
 end
