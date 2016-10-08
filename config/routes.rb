5e2d8a4e4a1b81ef511604a62d2695cb01971507
Adam Zolotarev <eugenezolo@gmail.com>
2016-10-08 17:29:07 -0400

    Add root route, use link_to shortcut

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # get 'events/movies'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'movies#index'
   get 'movies' => 'movies#index'
-  get 'movies/:id' => 'movies#show'
+  get 'movies/:id' => 'movies#show', as: 'movie'
 end
