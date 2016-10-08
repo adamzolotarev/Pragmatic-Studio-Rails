32c2c399e46f013464cc72063e6375aa0dcd574c
Adam Zolotarev <eugenezolo@gmail.com>
2016-10-08 17:10:30 -0400

    Add link_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'events/movies'
+  # get 'events/movies'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'movies' => 'movies#index'
