eefd2af24ab45b289cefe5433f95ebcc55846f72
Adam Zolotarev <eugenezolo@gmail.com>
2016-10-12 17:25:24 -0400

    Refactor to use movie_params private method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,9 +14,6 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    movie_params = params.require(:movie)
-    .permit(:title, :description, :rating, :released_on, :total_gross)
-
     @movie.update(movie_params)
     redirect_to @movie
   end
@@ -26,11 +23,14 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie_params = params.require(:movie)
-    .permit(:title, :description, :rating, :released_on, :total_gross)
-
     @movie = Movie.new(movie_params)
     @movie.save(movie_params)
     redirect_to @movie
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
   end
 end
