7e513f80f7c05b724ffb00ff138419cd67d86bde
Adam Zolotarev <eugenezolo@gmail.com>
2016-10-15 14:16:18 -0400

    Add movie model methods: released, flops

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ApplicationRecord
   def flop?
     total_gross.blank? || total_gross < 50_000_000
   end
+
+  def self.released
+    where('released_on <= ?', Time.now).order('released_on desc')
+  end
+
+  def self.hits
+    where('total_gross >= ?', 300_000_000).order(total_gross: :desc)
+  end
 end
--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -32,4 +32,12 @@ describe 'Viewing the list of movies' do
     expect(page).to have_text(movie1.description[0..10])
     expect(page).to have_text('$318,412,101.00')
   end
+
+  it 'does not show a movie that hasn\'t been yet released' do
+    movie = Movie.create(movie_attributes(released_on: 1.month.from_now))
+
+    visit movies_path
+
+    expect(page).not_to have_text(movie.title)
+  end
 end
