ddef566f07f9f95cb53459ade2cea47f451fa9ac
Adam Zolotarev <eugenezolo@gmail.com>
2016-10-20 21:30:09 -0400

    Add location to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021004321) do
+ActiveRecord::Schema.define(version: 20161021012821) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20161021004321) do
     t.integer  "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "city"
+    t.text     "state"
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
   end
 
