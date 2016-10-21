04d8b3995a73873bec87d7bb5f9558b1a2c81529
Adam Zolotarev <eugenezolo@gmail.com>
2016-10-20 21:24:20 -0400

    Add one-to-many model for Event-to-Reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015212859) do
+ActiveRecord::Schema.define(version: 20161021004321) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20161015212859) do
     t.string   "director"
     t.string   "duration"
     t.string   "image_file_name"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "stars"
+    t.text     "comment"
+    t.integer  "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_reviews_on_movie_id"
   end
 
 end
