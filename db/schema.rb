bc1d30ac14a87202472ab431ca34a0ca8a6f18b6
Adam Zolotarev <eugenezolo@gmail.com>
2016-10-30 08:46:58 -0400

    Add User with password; uncomment bcrypt; change ruby verions to 2.2.5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021012821) do
+ActiveRecord::Schema.define(version: 20161030120938) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 20161021012821) do
     t.text     "city"
     t.text     "state"
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
--- spec/support/attributes.rb
+++ spec/support/attributes.rb
@@ -1,7 +1,16 @@
 def review_attributes(overrides = {})
   {
-    name: "Roger Ebert",
+    name: 'Roger Ebert',
     stars: 3,
-    comment: "I laughed, I cried, I spilled my popcorn!"
+    comment: 'I laughed, I cried, I spilled my popcorn!'
+  }.merge(overrides)
+end
+
+def user_attributes(overrides = {})
+  {
+    name: 'Example User',
+    email: 'user@example.com',
+    password: 'secret',
+    password_confirmation: 'secret'
   }.merge(overrides)
 end
